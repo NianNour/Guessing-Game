1db4f5c55c08af32cda1675c82c9aad7037377b2
Nian Nourollahimoghadam <121342724+NianNour@users.noreply.github.com>
2023-06-30 11:40:14 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome !");
+            do
+            {
+                Console.WriteLine("Which number do i have in my mind ? Can you guess?");
+                int chance = 3;
+                int guessCount = 0;
+                Random random = new Random();
+                int computerNumber = random.Next(0, 11); //maling a random number 
+                while (guessCount < chance)
+                {
+                    int userNumber = Convert.ToInt32(Console.ReadLine());
+                    if (userNumber == computerNumber) // the win situation :
+                    {
+                        Console.WriteLine("You beat me!");
+                        break;
+                    }
+                    else if (computerNumber != userNumber && guessCount == 2) //checking the last input number : 
+                    {
+                        Console.WriteLine("\n **GAME OVER!** \n The number was : " + computerNumber);
+                    }
+                    guessCount++;
+                }
+                Console.WriteLine("Do want to try again? yes/no"); 
+            }
+            while (Console.ReadLine().ToUpper() == "YES");
+        }
+    }
+}
